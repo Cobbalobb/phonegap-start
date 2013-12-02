57b6e7189b52244ee0de1a7ff231428fc7415819
Cobbalobb <james.d.cobbett@gmail.com>
2013-12-02 17:33:17 +0000

    DB integration and JSON

--- www/js/users.js
+++ www/js/users.js
@@ -4,7 +4,7 @@ var users;
 
 function getuserList() {
 	console.log('test');
-	 $.getJSON(serviceURL + 'getuser.php', { dataType: 'jsonp'}, function(data) {
+	 $.getJSON(serviceURL + 'getuser.php?jsoncallback=processJSON', { dataType: 'jsonp'}, function(data) {
 	 	console.log(data.items.id);
 		users = data.items;
 		$('#name').append(data.items.name + ',');
