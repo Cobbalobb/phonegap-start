93a1caeab29deb9a31a1e70457e0cb499af31ee4
Cobbalobb <james.d.cobbett@gmail.com>
2014-03-19 17:17:03 +0000

    direct function

--- www/js/db.js
+++ www/js/db.js
@@ -1977,3 +1977,31 @@ FB.api(
 //   }
 // );
 }
+
+function direct(){
+  function queryDB(tx) {
+          //tx.executeSql('DROP TABLE IF EXISTS User');
+          tx.executeSql('SELECT first_name FROM User', [], querySuccess, errorCB);
+      }
+
+      function querySuccess(tx, results) {
+          console.log("Returned rows = " + results.rows.length);
+          var num = results.rows.length;
+          // this will be true since it was a select statement and so rowsAffected was 0
+          if (!results.rowsAffected) {
+              directToHome();
+              return false;
+          } else {
+              console.log('No rows affected!');
+          }
+          // for an insert statement, this property will return the ID of the last inserted row
+          console.log("Last inserted row ID = " + results.insertId);
+      }
+
+      function errorCB(err) {
+          goToLogin();
+      }
+
+      //var db = window.openDatabase("User", "1.0", "User DB", 1000000);
+      db.transaction(queryDB, errorCB);
+}
